df0b9786c0e16b96d13646a18174fe71412c2c41
Allison Stewart <amstewart@yahoo.com>
2022-02-25 12:19:36 -0500

    completed all tests

--- api/server.test.js
+++ api/server.test.js
@@ -25,7 +25,7 @@ describe('[GET] /api/jokes', () => {
     const res = await request(server).get('/api/jokes')
     expect(res.body).toEqual({ message: 'token required' })
   })
-  it('should return token invalid message if invalid token', async () => {
+  it('should return invalid credentials message if token invalid', async () => {
     request(server).post('/api/auth/register').send({ username: 'john', password: '123' })
     const user = await request(server).post('/api/auth/login').send({ username: 'john', password: '123', token: 'asdajkhdq 39293'})
     expect(user.body).toEqual({ message: 'invalid credentials' })
@@ -47,5 +47,25 @@ describe('[POST], /api/auth/register', () => {
   it('should return status code 201 if successful', async () => {
     const res = await request(server).post('/api/auth/register').send({ username: 'abc', password: '123' })
     expect(res.status).toBe(201)
+  })
+})
+
+describe('[POST], /api/auth/login', () => {
+  it('should return username and password required message if either is not sent', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'abc', password: '123' })
+    const res = await request(server).post('/api/auth/login').send({ username: '', password: '123' })
+    expect(res.body).toEqual({ message: 'username and password required'})
+  })
+  it('should return invalid credentials message if invalid username and/or password', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'abc', password: '123' })
+    let res = await request(server).post('/api/auth/login').send({ username: 'def', password: '123' })
+    expect(res.body).toEqual({ message: 'invalid credentials'})
+    res = await request(server).post('/api/auth/login').send({ username: 'abc', password: '456' })
+    expect(res.body).toEqual({ message: 'invalid credentials'})
+  })
+  it('should return status code 200 after successful login', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'abc', password: '123' })
+    const res = await request(server).post('/api/auth/login').send({ username: 'abc', password: '123' })
+    expect(res.status).toBe(200)
   })
 })
